dbd1cb533b4187fafe2d8e08537e4d0e2999b0c6
Laura <Laura@DESKTOP-BESSB6E>
2023-08-25 00:28:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Iveskite du skaicius: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (number > 0 && number2 > 0)
+{
+    Console.WriteLine("Abu skaiciai yra teigiami");
+}
+else if ((number < 0 && number2 > 0) || (number > 0 && number2 < 0))
+{ 
+    Console.WriteLine("Tik vienas skaicius yra teigiamas");
+}
+else
+{
+    Console.WriteLine("Ne vienas skaicius nera teigiamas");
+}
